9d73d07d44c837f4005a5f7100386f9d7becf5b3
irozalski <115094304+irozalski@users.noreply.github.com>
2024-03-13 22:17:51 +0100

    Second pump was added, further tests.

--- pouring_machine_ver3.ino
+++ pouring_machine_ver3.ino
@@ -5,9 +5,11 @@
 #define LED_COUNT 30
 #define LED_PIN    6
 
+//First pump Led change
 int inProgressLed1 = 0;
 int inProgressLed2 = 0;
 int inProgressLed3 = 0;
+
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -17,11 +19,17 @@ const int enableA = 3; //pwm pin
 const int in1 = 4;
 const int in2 = 5;
 
+// PUMP B
+
+const int enableB = 9; //pwm pin
+const int in3 = 8;
+const int in4 = 7;
+
 //STEPPER
-#define STEPPER_PIN_1 8
-#define STEPPER_PIN_2 9
-#define STEPPER_PIN_3 10
-#define STEPPER_PIN_4 11
+#define STEPPER_PIN_1 11
+#define STEPPER_PIN_2 10
+#define STEPPER_PIN_3 12
+#define STEPPER_PIN_4 13
 
 const int STEPS_PER_REVOLUTION = 2048;
 
@@ -44,12 +52,17 @@ int n = 1;
 
 boolean changestate = 0;
 
-boolean pumpStartDelay = false;
+boolean pumpStartDelay1 = false;
+boolean pumpStartDelay2 = false;
+
+boolean inProgressLedForPump1Off = false;
 
 //MILLIS
 unsigned long previousMillis = 0;  // Variable to store the last time the command was executed
-const unsigned long delayTime = 5000;  // Time to delay in milliseconds
-const unsigned long pumpDelayTime = 1000;
+const unsigned long delayTime = 3000;  // Time to delay in milliseconds
+const unsigned long delayTime2 = 6000;  // Time to delay in milliseconds
+const unsigned long pumpDelayTime1 = 1000; //Start after 1 second and ends after 3 sec: 2 sec work time
+const unsigned long pumpDelayTime2 = 4000; //Start after 4 seconds and ends after 6 sec: 2 sec work time
 
 
 // Initialize the AccelStepper object with the appropriate pins, steps per revolution, motor speed, and acceleration
@@ -66,6 +79,11 @@ void setup() {
   pinMode(enableA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+
+  //PUMP B
+  pinMode(enableB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
   
   //BUTTONS
   pinMode(START_BUTTON_PIN, INPUT_PULLUP);
@@ -86,7 +104,7 @@ void loop() {
   int buttonState3 = digitalRead(BUTTON_PIN3);
 
   analogWrite (enableA, 255); //Voltage regulation of motorA
-  
+  analogWrite (enableB, 255); //Voltage regulation of motorB
 
    switch(buttonState1)
 {
@@ -96,11 +114,17 @@ case LOW:
       case 1:
       led_on(0,3,255,255,0);
       break;
+      
+      case 2:
+      led_on(0,3,255,0,255);
+      break;
+      
       default:
       led_on(0,3,0,255,0);
       break;
-    }
+    }  
     break;
+    
   default:
   led_off(0,3);
   break;
@@ -114,11 +138,17 @@ case LOW:
       case 1:
       led_on(3,6,255,255,0);
       break;
+
+      case 2:
+      led_on(3,6,255,0,255);
+      break;
+      
       default:
       led_on(3,6,0,255,0);
       break;
     }
     break;
+    
   default:
   led_off(3,6);
   break;
@@ -132,10 +162,16 @@ case LOW:
       case 1:
       led_on(6,9,255,255,0);
       break;
+
+      case 2:
+      led_on(6,9,255,0,255);
+      break;
+      
       default:
       led_on(6,9,0,255,0);
       break;
     }
+    
     break;
   default:
   led_off(6,9);
@@ -153,7 +189,9 @@ case LOW:
     stepper.moveTo(fullCircle);
     stepper.setSpeed(MAX_SPEED);
     unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= pumpDelayTime && pumpStartDelay == true){
+
+    //First pump
+    if (currentMillis - previousMillis >= pumpDelayTime1 && pumpStartDelay1 == true){
        //Pump start
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
@@ -169,12 +207,50 @@ case LOW:
         if (n==4){
         inProgressLed3 = 1;
         }
-        pumpStartDelay = false;
+        pumpStartDelay1 = false;
+        inProgressLedForPump1Off = true;
     }
+    
+    //Second pump
+    if (currentMillis - previousMillis >= pumpDelayTime2 && pumpStartDelay2 == true){
+       //Pump start
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+
+        //Leds change 
+        // have to check value of n incremented by 1 because of n++ at the and of the loop that is always added
+        if (n==2){
+        inProgressLed1 = 2;
+        }
+        if (n==3){
+        inProgressLed2 = 2;
+        }
+        if (n==4){
+        inProgressLed3 = 2;
+        }
+        pumpStartDelay2 = false;
+    }
+
+    //Pump 1 Stop
     if (currentMillis - previousMillis >= delayTime){
-    //Pump stop
+    //Pump 1 stop
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
+    
+    if (inProgressLedForPump1Off == true){
+    //Leds reset, it should work only once 
+    inProgressLed1 = 0;
+    inProgressLed2 = 0;
+    inProgressLed3 = 0;
+    inProgressLedForPump1Off = false;
+      }
+    }
+
+    //Pump 2 Stop and continuation of program
+    if (currentMillis - previousMillis >= delayTime2){
+    //Pump 2 stop
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
     //Leds reset
     inProgressLed1 = 0;
     inProgressLed2 = 0;
@@ -186,7 +262,8 @@ case LOW:
       if (n==1 && buttonState1 == LOW){
         stepper.stop();   
         previousMillis = currentMillis; 
-        pumpStartDelay = true;
+        pumpStartDelay1 = true;
+        pumpStartDelay2 = true;
        
 
         
@@ -196,13 +273,15 @@ case LOW:
         stepper.stop();
         previousMillis = currentMillis; 
         
-        pumpStartDelay = true;
+        pumpStartDelay1 = true;
+        pumpStartDelay2 = true;
       }
       if (n==3 && buttonState3 == LOW){
         stepper.stop();
         previousMillis = currentMillis;
          
-        pumpStartDelay = true;
+        pumpStartDelay1 = true;
+        pumpStartDelay2 = true;
       }
       n++;  
     }
